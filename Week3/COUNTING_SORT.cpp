5ca45157f135e7c5464ff05281669d6bc8328f52
Chu Huy Quang <111046219+pynterest83@users.noreply.github.com>
2023-03-01 09:32:16 +0700

    Create COUNTING_SORT.cpp

---
+++ Week3/COUNTING_SORT.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define FORU(i , a , b) for(int i = a ; i <= b ; i++)
+#define FORD(i , a , b) for(int i = a ; i >= b ; i--)
+using namespace std;
+const int MAX = 1e6 + 10, MOD = 1e9 + 7;
+void unset(int &t, int pos)
+{
+    t &= (~(1ll << (pos - 1)));
+}
+void onset(int &t, int pos)
+{
+    t |= (1ll << (pos - 1));
+}
+int getbit(int i,int k)
+{
+    return (k >> (i - 1)) % 2;
+}
+void minimize(int &x, int y)
+{
+    x = min(x, y);
+}
+void maximize(int &x, int y)
+{
+    x = max(x, y);
+}
+int ADD(int x, int y)
+{
+    return (x + y) % MOD;
+}
+int MINUS(int x, int y)
+{
+    x %= MOD, y %= MOD;
+    if (x < y) x += MOD;
+    return (x - y) % MOD;
+}
+//========================================================================0========================================================================//
+int n, a[MAX], cnt[MAX], m, res[MAX];
+void solve()
+{
+    for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
+    for (int i = 1; i <= n; i++) res[cnt[a[i]]--] = a[i];
+    for (int i = 1; i <= n; i++) cout << res[i] << " ";
+}
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("t.inp", "r", stdin);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        cnt[a[i]]++;
+        maximize(m, a[i]);
+    }
+    solve();
+    return 0;
+}
